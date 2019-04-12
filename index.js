4bde8d4aa5656f661dacb24c2f6198073e5dfe9e
Ethan <ethandeguire@gmail.com>
2019-04-11 22:45:42 -0400

    fixed webart

--- index.js
+++ index.js
@@ -1,31 +1,15 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const app = express();
-var fs = require('fs');
-var path = require('path');
+const app = express()
+var path = require('path')
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('client'));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('client'))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/hub/index.html'));
-});
-app.get('/stats', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/stats/index.html'));
-});
-app.get('/webart', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/WebArt/index.html'));
-});
-app.get('/minmax', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/minmax/index.html'));
-});
-app.get('/efield', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/efield/index.html'));
-});
-app.get('/warsim', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/warSim/index.html'));
-});
+  res.sendFile(path.join(__dirname, '/client/hub/index.html'))
+})
 
-app.listen(3000, () => console.log('server started'));
+app.listen(3000, () => console.log('server started'))
