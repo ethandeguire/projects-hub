d02506c4ecd9f51001474be7cb7a53155b95530c
Ethan <ethandeguire@gmail.com>
2019-05-12 23:11:46 -0400

    final fix
    if this doesnt work

--- client/p5.graphing/script.js
+++ client/p5.graphing/script.js
@@ -30,3 +30,10 @@ function showHideDiv(ele) {
     return false;
   }
 }
+
+function loadscript(url) {
+  var script = document.createElement("script");  // create a script DOM node
+  script.src = url;  // set its src to the provided URL
+
+  document.head.appendChild(script);  // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
+}
