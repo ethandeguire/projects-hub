2eab2ec1fb399c40c99f32062a564f84ecccd96b
Ethan <ethandeguire@gmail.com>
2019-08-03 21:11:45 -0400

    test

--- functions/test.js
+++ functions/test.js
@@ -1,9 +1,18 @@
+const faunadb = require('faunadb'), q = faunadb.query;
+const client = new faunadb.Client({ secret: 'fnADU6udygACDT_EIqE1xy887pb_LSYTv-VE53_P' });
+
+
 exports.handler = function(event, context, callback) {
-  console.log('event', event)
-  console.log('context', context)
+  let res
+  
+  var createP = client.query(q.Create(q.Collection('scores'), { data: { testField: 'testValue' } }));
+  createP.then(function(response) {
+    console.log(response.ref); // Would log the ref to console.
+    res = response.ref
+  });
 
   callback(null, {
     statusCode: 200,
-    body: "Hello, World ⊂◉‿◉つ"
+    body: "Hello, World ⊂◉‿◉つ" + res
   });
 }
